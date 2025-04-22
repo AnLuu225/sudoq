eefba40b8b7145084bd2f7373cbdf06f1f021fcb
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2025-04-22 14:01:23 -0600

    commit

--- app/play_page/page.jsx
+++ app/play_page/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { Container, Typography, Button, Grid, CircularProgress } from "@mui/material";
-import { fetchSudokuPuzzle } from "@/api/sudoku";
+import { fetchSudokuPuzzle } from "../../api/sudoku";
 import { useRouter } from "next/navigation";
 
 const PlayPage = () => {
