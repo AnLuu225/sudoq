543aadb6bd44d63ad6aa43c896961d37a7186d23
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2025-04-22 12:43:22 -0600

    commit

--- app/page.js
+++ app/page.js
@@ -90,21 +90,21 @@ const Homepage = () => {
 
         <Grid container spacing={2} marginTop={4}>
         <Grid item xs={4}>
-            <Link href="\play_page.js" passHref>
+            <Link href="/play_page" passHref>
             <Button variant="contained" fullWidth>
                 Play Sudoku
             </Button>
             </Link>
         </Grid>
         <Grid item xs={4}>
-            <Link href="\create_page.js" passHref>
+            <Link href="/create_page" passHref>
             <Button variant="contained" fullWidth>
                 Create Puzzle
             </Button>
             </Link>
         </Grid>
         <Grid item xs={4}>
-            <Link href="\about_page.js" passHref>
+            <Link href="/about_page" passHref>
             <Button variant="contained" fullWidth>
                 About
             </Button>
