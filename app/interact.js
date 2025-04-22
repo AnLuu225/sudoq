6e0e75c3b361a611ff9e23148ba9af81e29f5811
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2025-04-22 03:45:22 -0600

    commit

--- app/interact.js
+++ app/interact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './app/page';
 import PlayPage from './app/play_page';
-import CreateSudokuPage from './app/create_page';
+import CreatePage from './app/create_page';
 import AboutPage from './app/about_page';
 
 const Clicker = () => {
@@ -11,7 +11,7 @@ const Clicker = () => {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/play" element={<PlayPage />} />
-        <Route path="/create" element={<CreateSudokuPage />} />
+        <Route path="/create" element={<CreatePage />} />
         <Route path="/about" element={<AboutPage />} />
       </Routes>
     </Router>
