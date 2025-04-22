12ef3dd9667396fd28eb80afff14e32e00d5822f
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2025-04-22 12:24:30 -0600

    commit

--- app/page.js
+++ app/page.js
@@ -91,6 +91,7 @@ const Homepage = () => {
         <Grid container spacing={2} marginTop={4}>
         <Grid item xs={4}>
             <Link href="\play\play_page" passHref>
+            
             <Button variant="contained" fullWidth>
                 Play Sudoku
             </Button>
